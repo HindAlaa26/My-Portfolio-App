d9d4243a5ec2cd5f6d74850c1f2ed538970c8704
HindAlaa26 <hindalaa26.com>
2024-08-15 19:47:32 +0300

    web view updated

--- lib/shared_component/WebView.dart
+++ lib/shared_component/WebView.dart
@@ -1,19 +1,62 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-class ShowWebView extends StatelessWidget {
+class ShowWebView extends StatefulWidget {
   final String url;
 
   const ShowWebView({super.key, required this.url});
 
   @override
+  State<ShowWebView> createState() => _ShowWebViewState();
+}
+
+class _ShowWebViewState extends State<ShowWebView> {
+  late final WebViewController controller;
+  var loadingPercentage = 0;
+  @override
+  void initState() {
+    super.initState();
+    controller = WebViewController()
+      ..setNavigationDelegate(NavigationDelegate(
+        onPageStarted: (url) {
+          setState(() {
+            loadingPercentage = 0;
+          });
+        },
+        onProgress: (progress) {
+          setState(() {
+            loadingPercentage = progress;
+          });
+        },
+        onPageFinished: (url) {
+          setState(() {
+            loadingPercentage = 100;
+          });
+        },
+      ))
+      ..loadRequest(
+        Uri.parse(widget.url),
+      );
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('GitHub Page'),
-        ),
-        body: WebView(
-          initialUrl: url,
-        ));
+      appBar: AppBar(
+        title: const Text('GitHub Page'),
+      ),
+      body: Stack(
+        children: [
+          WebViewWidget(
+            controller: controller,
+          ),
+          if (loadingPercentage < 100)
+            LinearProgressIndicator(
+              color: Colors.amber,
+              value: loadingPercentage / 100.0,
+            ),
+        ],
+      ),
+    );
   }
 }
